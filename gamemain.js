1972b8704ef2c73acc729c22929d9ca09226703a
huahua1018 <94890109+huahua1018@users.noreply.github.com>
2022-01-16 20:54:20 +0800

    Update gamemain.js

--- gamemain.js
+++ gamemain.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
     localStorage.clear();
     $("#fairydia").hide();
+    $("#piecepaper").hide();
     $("#phonedia").hide();
     $("#deskAndFish").hide();
     $("#loadingimg").show();
